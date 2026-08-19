94134dfc34ffda65539ef4c79c6ef0b16ea863af
nhattruc512-hub <nhattruc512@gmail.com>
2026-08-19 22:42:30 +0700

    Refresh app cache for shared cloud history

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='r971-staff-v5';
+const CACHE='r971-staff-v6';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./debt-fix.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
