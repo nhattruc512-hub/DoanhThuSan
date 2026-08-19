533f979a85680fd586f57de372ef33a30eb712cb
nhattruc512-hub <nhattruc512@gmail.com>
2026-08-20 06:21:29 +0700

    Refresh manager attendance controls cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='r971-staff-v11';
+const CACHE='r971-staff-v12';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./debt-fix.js','./admin.js','./live-shift.js','./attendance.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
