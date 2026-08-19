54713c3c658c90847637787d8c573ec6fe9b91e0
nhattruc512-hub <nhattruc512@gmail.com>
2026-08-19 23:10:20 +0700

    Refresh cache for manager area

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='r971-staff-v6';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./debt-fix.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='r971-staff-v7';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./debt-fix.js','./admin.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
